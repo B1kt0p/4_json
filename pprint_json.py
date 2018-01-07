924cf2905ee9e09971d8a7e9cd8610a7802cac66
buktop <buktop>
2018-01-07 23:21:58 +0900

    Исправлены ошибки

--- pprint_json.py
+++ pprint_json.py
@@ -1,5 +1,7 @@
 import json
-import os, sys
+import os
+import sys
+
 
 def read_json_filenames_from_args():
     file_path = []
@@ -13,7 +15,7 @@ def read_json_filenames_from_args():
             return file_path
         else:
             print("Не найден не один файл")
-            exit(0)
+            return None
     except IndentationError:
         pass
 
@@ -21,22 +23,23 @@ def read_json_filenames_from_args():
 def load_json_from_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
         try:
-            data=json.loads(f.read())
-        except:
-            data=None
-    return data
+            json_data = json.loads(f.read())
+        except json.decoder.JSONDecodeError:
+            json_data = None
+    return json_data
 
-def pretty_print_json(json_loads):
-    print(json.dumps(json_loads, indent=4, sort_keys=True, ensure_ascii=False))
+
+def pretty_print_json(data):
+    print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
+
 
 if __name__ == '__main__':
     path_files = read_json_filenames_from_args()
-    for path in path_files:
-        data=load_json_from_file(path)
-    if data:
-        pretty_print_json(data)
-    else:
-        print("Файл {} некоретный".format(path))
-
+    if path_files:
+        for path in path_files:
+            json_data = load_json_from_file(path)
+        if json_data:
+            pretty_print_json(json_data)
+        else:
+            print("Файл {} некоретный".format(path))
     print("Программа завершена.")
-
