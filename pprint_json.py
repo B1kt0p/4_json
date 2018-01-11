199257ef404a09180ae467ee8de4c02135f82af1
buktop <buktop>
2018-01-11 20:03:17 +0900

    Исправлены названия переменных

--- pprint_json.py
+++ pprint_json.py
@@ -20,26 +20,26 @@ def read_json_filenames_from_args():
         pass
 
 
-def load_json_from_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as f:
+def load_json_from_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
         try:
-            data = json.loads(f.read())
+            json_content = json.loads(f.read())
         except json.decoder.JSONDecodeError:
-            data = None
-    return data
+            json_content = None
+    return json_content
 
 
-def pretty_print_json(data_json):
-    print(json.dumps(data_json, indent=4, sort_keys=True, ensure_ascii=False))
+def pretty_print_json(json_content):
+    print(json.dumps(json_content, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     file_paths = read_json_filenames_from_args()
     if file_paths:
         for file_path in file_paths:
-            data_json = load_json_from_file(file_path)
-            if data_json:
-                pretty_print_json(data_json)
+            json_content = load_json_from_file(file_path)
+            if json_content:
+                pretty_print_json(json_content)
             else:
                 print("Файл {} некоретный".format(file_path))
     print("Программа завершена.")
