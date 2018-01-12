712f198878359c41b358e58a58f8ee35b92f728f
buktop <buktop>
2018-01-12 12:49:17 +0900

    Исправлены замечания от 11 января 2018 г.

--- pprint_json.py
+++ pprint_json.py
@@ -1,45 +1,38 @@
 import json
-import os
 import sys
 
 
 def read_json_filenames_from_args():
-    path_files = []
     try:
-        for path_to_json in sys.argv[1:]:
-            if os.path.exists(path_to_json):
-                path_files.append(path_to_json)
-            else:
-                print("Файл {} не найден".format(path_to_json ))
-        if path_files:
-            return path_files
-        else:
-            print("Не найден не один файл")
-            return None
-    except IndentationError:
-        pass
+        path_to_json = sys.argv[1]
+        return path_to_json
+    except IndexError:
+        return None
 
 
 def load_json_from_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        try:
-            json_content = json.loads(f.read())
-        except json.decoder.JSONDecodeError:
-            json_content = None
-    return json_content
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return json.loads(file.read())
+    except json.decoder.JSONDecodeError:
+        return None
+    except FileNotFoundError:
+        return None
 
 
 def pretty_print_json(json_content):
-    print(json.dumps(json_content, indent=4, sort_keys=True, ensure_ascii=False))
+    print(json.dumps(json_content, indent=4,
+                     sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    file_paths = read_json_filenames_from_args()
-    if file_paths:
-        for file_path in file_paths:
-            json_content = load_json_from_file(file_path)
-            if json_content:
-                pretty_print_json(json_content)
-            else:
-                print("Файл {} некоретный".format(file_path))
+    file_path = read_json_filenames_from_args()
+    if file_path:
+        json_content = load_json_from_file(file_path)
+        if json_content:
+            pretty_print_json(json_content)
+        else:
+            print("Файл {} некоретный или не существует".format(file_path))
+    else:
+        print("Не введено имя файла")
     print("Программа завершена.")
