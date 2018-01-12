6e5de8a447a06cc135b71db297a8bdee4997d4ee
buktop <buktop>
2018-01-12 18:03:10 +0900

    Устранены замечания от 12.01.2018

--- pprint_json.py
+++ pprint_json.py
@@ -2,37 +2,30 @@ import json
 import sys
 
 
-def read_json_filenames_from_args():
-    try:
-        path_to_json = sys.argv[1]
-        return path_to_json
-    except IndexError:
-        return None
-
-
 def load_json_from_file(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             return json.loads(file.read())
-    except json.decoder.JSONDecodeError:
-        return None
-    except FileNotFoundError:
+    except (json.decoder.JSONDecodeError, FileNotFoundError):
         return None
 
 
 def pretty_print_json(json_content):
-    print(json.dumps(json_content, indent=4,
-                     sort_keys=True, ensure_ascii=False))
+    print(json.dumps(
+        json_content,
+        indent=4, sort_keys=True,
+        ensure_ascii=False
+    ))
 
 
 if __name__ == '__main__':
-    file_path = read_json_filenames_from_args()
-    if file_path:
+    if len(sys.argv) == 2:
+        file_path = sys.argv[1]
         json_content = load_json_from_file(file_path)
         if json_content:
             pretty_print_json(json_content)
         else:
             print("Файл {} некоретный или не существует".format(file_path))
     else:
-        print("Не введено имя файла")
+        print("Неправильный формат команды. Обратитесь к README.md")
     print("Программа завершена.")
